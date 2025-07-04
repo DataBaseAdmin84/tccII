f889d4bbeeae8241dacb7f5338a1ad02d5ee9a58
Emerson Souza <140563230+EmersonDba84@users.noreply.github.com>
2025-07-04 18:12:46 -0300

    Ajustes filtro (#13)
    Ajustado filtros tela cursos

--- src/main/java/com/filtro/FiltroCurso.java
+++ src/main/java/com/filtro/FiltroCurso.java
@@ -1,5 +1,6 @@
 package com.filtro;
 
+import com.dto.CursoDTO;
 import com.model.Curso;
 import jakarta.persistence.criteria.Predicate;
 import org.springframework.data.jpa.domain.Specification;
@@ -12,6 +13,8 @@ import java.util.List;
 public class FiltroCurso {
     private Long id;
     private String titulo;
+    private String descricao;
+    private String nomeProfessor;
 
     public Specification<Curso> toSpecification() {
         return (root, query, criteriaBuilder) -> {
@@ -25,14 +28,30 @@ public class FiltroCurso {
                 predicates.add(criteriaBuilder.like(criteriaBuilder.upper(root.get("titulo")), "%"+titulo.toUpperCase()+"%"));
             }
 
+            if(!StringUtils.isEmpty(nomeProfessor)) {
+                predicates.add(criteriaBuilder.like(criteriaBuilder.upper(root.join("professor").get("nomeCompleto")), "%" + nomeProfessor.toUpperCase() + "%"));
+            }
+
+            if(!StringUtils.isEmpty(descricao)) {
+                predicates.add(criteriaBuilder.like(criteriaBuilder.upper(root.get("descricao")), "%"+descricao.toUpperCase()+"%"));
+            }
+
             return criteriaBuilder.and(predicates.toArray(new Predicate[0]));
         };
     }
 
-    public void preencheFiltro(Model model) {
-        var titulo = model.getAttribute("titulo");
-        if(titulo != null && !titulo.toString().isEmpty()) {
-            this.titulo = titulo.toString();
+    public void preencheFiltro(CursoDTO dto) {
+        if(dto.getId() != null){
+            setId(dto.getId());
+        }
+        if(dto.getTitulo() != null && !dto.getTitulo().isEmpty()){
+            setTitulo(dto.getTitulo());
+        }
+        if(dto.getProfessorNome() != null && !dto.getProfessorNome().isEmpty()){
+            setNomeProfessor(dto.getProfessorNome());
+        }
+        if(dto.getDescricao() != null && !dto.getDescricao().isEmpty()){
+            setDescricao(dto.getDescricao());
         }
     }
 
@@ -50,5 +69,21 @@ public class FiltroCurso {
 
     public void setTitulo(String titulo) {
         this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getNomeProfessor() {
+        return nomeProfessor;
+    }
+
+    public void setNomeProfessor(String nomeProfessor) {
+        this.nomeProfessor = nomeProfessor;
     }
 }
