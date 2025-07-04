1ffad4680e73e6aa1d3e7c53a2bc369979b72020
EmersonDba84 <emersonpds84@gmail.com>
2025-07-04 18:12:03 -0300

    Ajustes filtro
    Ajustado filtros tela cursos

--- src/main/java/com/controller/CursoController.java
+++ src/main/java/com/controller/CursoController.java
@@ -14,15 +14,12 @@ import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.io.IOException;
 import java.text.ParseException;
-import java.util.List;
+import java.util.ArrayList;
 
 @Controller
 public class CursoController {
@@ -79,12 +76,16 @@ public class CursoController {
     }
 
     @GetMapping("/cursos")
-    public String listarCursos(Model model) {
+    public String listarCursos(CursoDTO dto, Model model) {
         var filtro = new FiltroCurso();
-        filtro.preencheFiltro(model);
+        filtro.preencheFiltro(dto);//metodo para preencher o filtro criteriaBuilder (implementação filtro dafault do Spring boot)
 
         var cursos = cursoRepository.findAll(filtro.toSpecification());
-      //  model.addAttribute("cursos", C);
+        var listCurso = new ArrayList<CursoDTO>();
+        for(Curso curso : cursos){
+            listCurso.add(CursoDTO.toDto(curso));
+        }
+        model.addAttribute("cursos", listCurso);
         return "curso/cursos";
     }
 
